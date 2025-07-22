aa0070ae97d19e4dc83ba0a82168e055dfb019d1
Jamie Li <>
2025-07-22 16:26:08 +0800

    [設定] 初始化安裝

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import jobList from './constants/jobList'
 import educationList from './constants/educationList'
 import salaryList from './constants/salaryList'
 import { Factory, Model, Server } from 'miragejs'
+import './styles/global.scss'
 
 const filterFormat = (data, companyName, educationLevel, salaryLevel) => {
   let result = data
@@ -33,22 +34,22 @@ new Server({
       companyName(i) {
         return jobList[i].companyName
       },
-      jobTitle(i){
+      jobTitle(i) {
         return jobList[i].jobTitle
       },
-      educationId(i){
+      educationId(i) {
         return jobList[i].educationId
       },
-      salaryId(i){
+      salaryId(i) {
         return jobList[i].salaryId
       },
-      preview(i){
+      preview(i) {
         return jobList[i].preview
       },
-      companyPhoto(i){
+      companyPhoto(i) {
         return jobList[i].companyPhoto
       },
-      description(i){
+      description(i) {
         return jobList[i].description
       },
     }),
@@ -58,7 +59,7 @@ new Server({
       },
       label(i) {
         return educationList[i].label
-      }
+      },
     }),
     salaryList: Factory.extend({
       id(i) {
@@ -66,11 +67,11 @@ new Server({
       },
       label(i) {
         return salaryList[i].label
-      }
+      },
     }),
   },
   seeds(server) {
-    server.createList("jobList", jobList.length)
+    server.createList('jobList', jobList.length)
     server.createList('educationList', educationList.length)
     server.createList('salaryList', salaryList.length)
   },
@@ -87,51 +88,49 @@ new Server({
       const prePage = Number(request.queryParams.pre_page)
       const page = Number(request.queryParams.page)
 
-      let data = schema.jobLists.all().models
-        .map(({ attrs: { companyPhoto, description, ...rest}}) => rest)
+      let data = schema.jobLists
+        .all()
+        .models.map(({ attrs: { companyPhoto, description, ...rest } }) => rest)
 
-      if(!isNaN(prePage) && !isNaN(page)) {
+      if (!isNaN(prePage) && !isNaN(page)) {
         const startIndex = (page - 1) * prePage
         const endIndex = startIndex + prePage
         const filterData = filterFormat(data, companyName, educationLevel, salaryLevel)
-        const resultData = filterData.slice(startIndex, endIndex) 
+        const resultData = filterData.slice(startIndex, endIndex)
         return {
           data: resultData,
-          total: filterData.length
+          total: filterData.length,
         }
       } else {
         const result = filterFormat(data, companyName, educationLevel, salaryLevel)
         return {
           data: result,
-          total: result.length
+          total: result.length,
         }
       }
     })
 
-    this.get('/educationLevelList', (schema) => {
-      return schema.educationLists.all().models
-        .map(item => item.attrs)
+    this.get('/educationLevelList', schema => {
+      return schema.educationLists.all().models.map(item => item.attrs)
     })
 
-    this.get('/salaryLevelList', (schema) => {
-      return schema.salaryLists.all().models
-        .map(item => item.attrs)
+    this.get('/salaryLevelList', schema => {
+      return schema.salaryLists.all().models.map(item => item.attrs)
     })
 
     this.get('/jobs/:id', (schema, request) => {
       const id = request.params.id
-      const data = schema.jobLists.all().models
-        .find(item => item.id === id)
+      const data = schema.jobLists.all().models.find(item => item.id === id)
 
-      if(data) {
+      if (data) {
         const { preview, educationId, salaryId, ...rest } = data.attrs
         return rest
       } else {
         return []
       }
     })
-  }
+  },
 })
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
